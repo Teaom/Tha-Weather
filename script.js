0a4610a02dad4baeec9ee73400a37cc692c3f156
Teaom <dclarkjenkins87@gmail.com>
2023-04-12 15:50:18 -0600

    color scheme in CSS, optimize JS

--- script.js
+++ script.js
@@ -1,207 +1,95 @@
-let searchForm = document.getElementById("searchForm");
-let searchInput = document.getElementById("searchInput");
-let current = document.getElementById("current");
-let forecast = document.getElementById("forecast");
-let searchHistory = document.getElementById("searchHistory");
+const searchForm = document.getElementById("searchForm");
+const searchInput = document.getElementById("searchInput");
+const current = document.getElementById("current");
+const forecast = document.getElementById("forecast");
+const searchHistory = document.getElementById("searchHistory");
 
-let APIKey = "620c253d0c9e6dc17da38e9136d39bd1";
+const APIKey = "620c253d0c9e6dc17da38e9136d39bd1";
 
+let storedSearches = JSON.parse(localStorage.getItem("searches")) || [];
 
-//Local Storage
-
-let storedSearches;
-
-function renderLocalStorage () {
+function renderLocalStorage() {
     searchHistory.innerHTML = "";
 
-    for (let i = 0; i < storedSearches.length; i++) {
-        let searchHistoryDiv = document.createElement("div");
-        searchHistoryDiv.className = "searchHistoryDiv";
-      
-        let searchHistoryBtn = document.createElement("button");
-       
-       searchHistoryBtn.textContent = storedSearches [i];
-
-       searchHistoryDiv.append(searchHistoryBtn);
-       searchHistory.append(searchHistoryBtn);
-        
-    }
+    storedSearches.forEach(search => {
+        const searchHistoryBtn = document.createElement("button");
+        searchHistoryBtn.className = "searchHistoryDiv";
+        searchHistoryBtn.textContent = search;
+        searchHistory.append(searchHistoryBtn);
+    });
 }
 
- function getLocalStorage() {
-    storedSearches = [];
-    storedSearches = JSON.parse(localStorage.getItem("searches")) || [];
-    renderLocalStorage();
- }
-
-    getLocalStorage();
-
-    //current weather
-    function renderCurrentWeather(data) {
-        current.innerHTML = "";
-        let name = document.createElement("div");
-        name.textContent = data.name;
-        current.append(name);
-
-        let date = document.createElement("div");
-        date.textContent = new Date(data.dt * 1000).toLocaleTimeString("en-US", {
-            weekday: "long", 
+function renderCurrentWeather(data) {
+    current.innerHTML = `
+        <div>${data.name}</div>
+        <div>${new Date(data.dt * 1000).toLocaleTimeString("en-US", {
+            weekday: "long",
             year: "numeric",
             month: "short",
             day: "numeric",
-        });
-        current.append(date);
-
-        let temp = document.createElement("div");
-        temp.textContent = data.main.temp;
-        current.append(temp);
-
-        let humidity = document.createElement("div");
-        humidity.textContent = data.main.humidity;
-        current.append(humidity);
-
-        let windSpeed = document.createElement("div");
-        windSpeed.textContent = data.wind.speed;
-        current.append(windSpeed);
-
-        let icon = document.createElement("img");
-        icon.src = "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
-        current.append(icon);
-        
-        
-    }
-    
-    function displayCurrent(name) {
-        let cityName = name;
-        let cityQuery =
-        "https://api.openweathermap.org/data/2.5/weather?q=" +
-        cityName + 
-        "&units=imperial" +
-        "&appid=" +
-        APIKey;
-
-    fetch(cityQuery)
-        .then(function (res) {
-            return res.json()
-    })
-    .then(function (data) {
-        console.log(data); 
-        renderCurrentWeather(data);
-    })
-    .catch(function (err) {
-         console.log(err);
-    });
-    
+        })}</div>
+        <div>${data.main.temp}</div>
+        <div>${data.main.humidity}</div>
+        <div>${data.wind.speed}</div>
+        <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" />
+    `;
 }
 
+function displayCurrent(name) {
+    const cityQuery = `https://api.openweathermap.org/data/2.5/weather?q=${name}&units=imperial&appid=${APIKey}`;
 
-//5 Day
-
-let forecastHigh = -100;
-
-let forecastLow = 200;
-
-let avgWindSpeed = 0;
-
-let avgHumidity = 0;
-
-let forcastIconSrc;
-
+    fetch(cityQuery)
+        .then(res => res.json())
+        .then(data => renderCurrentWeather(data))
+        .catch(err => console.log(err));
+}
 
 function renderForecast(data) {
-    forecast.innerHTML = "";
-    let infoIndex = [7, 15, 23, 31, 39];
-    let iconIndex = []
-    for (let i = 0; i < data.list.length; i++) {
-        let forecastCard = document.createElement("div");
-        let forecastIcon = document.createElement("img");
+    const infoIndex = [7, 15, 23, 31, 39];
 
-    if(data.list[i].main.temp_max > forecastHigh) {
-         forecastHigh = data.list[i].main.temp_max;
-    }
-    if(data.list[i].main.temp_min < forecastLow) {
-        forecastLow = data.list[i].main.temp_min;
-    }
-
-     avgHumidity = avgHumidity + data.list[i].main.humidity;
-
-     avgWindSpeed = avgWindSpeed + data.list[i].wind.speed;
-     
-     if (infoIndex.includes(i)) {
-        console.log("i", i);
-    highTemp = document.createElement("div");
-    highTemp.textContent = "High: " + parseInt(forecastHigh);
-    forecastCard.append(highTemp);
-    forecastHigh = -100;
-
-    lowTemp = document.createElement("div");
-    lowTemp.textContent = "Low: " + parseInt(forecastLow);
-    forecastCard.append(lowTemp);
-    forecastLow = 200;
-
-    humidity = document.createElement("div");
-    humidity.textContent = "Humidity: " + parseInt(avgHumidity / 8);
-    forecastCard.append(humidity);
-    avgHumidity = 0;
-
-    windSpeed = document.createElement("div");
-    windSpeed.textContent = "Wind: " + parseInt(avgWindSpeed / 8);
-    forecastCard.append(windSpeed);
-    avgWindSpeed = 0;
-
-    forecastIcon.src =
-    "https://openweathermap.org/img/wn/" +
-    data.list[i].weather[0].icon +
-    "@2x.png";
-    forecastCard.append(forecastIcon);
-
-
-    forecast.append(forecastCard);
-     }
- } 
- }
+    forecast.innerHTML = data.list.reduce((acc, forecast, i) => {
+        if (infoIndex.includes(i)) {
+            return acc + `
+                <div>
+                    <div>High: ${parseInt(forecast.main.temp_max)}</div>
+                    <div>Low: ${parseInt(forecast.main.temp_min)}</div>
+                    <div>Humidity: ${parseInt(forecast.main.humidity)}</div>
+                    <div>Wind: ${parseInt(forecast.wind.speed)}</div>
+                    <img src="https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" />
+                </div>
+            `;
+        }
+        return acc;
+    }, '');
+}
 
 function displayForecast(name) {
-    let cityName = name;
-        let forecastQuery =
-        "https://api.openweathermap.org/data/2.5/forecast?q=" +
-        cityName + 
-        "&units=imperial" +
-        "&appid=" +
-        APIKey;
+    const forecastQuery = `https://api.openweathermap.org/data/2.5/forecast?q=${name}&units=imperial&appid=${APIKey}`;
 
     fetch(forecastQuery)
-        .then(function (res) {
-            return res.json();
-    })
-    .then(function (data) {
-        console.log(data); 
-        renderForecast(data);
-    })
-    .catch(function (err) {
-         console.log(err);
-    });
+        .then(res => res.json())
+        .then(data => renderForecast(data))
+        .catch(err => console.log(err));
 }
 
 function searchCity(e) {
     e.preventDefault();
-    if (searchInput.value !== "") {
-    displayCurrent(searchInput.value);
-    displayForecast(searchInput.value);
-    storedSearches.unshift(searchInput.value);
-    localStorage.setItem("searches", JSON.stringify(storedSearches));
-    getLocalStorage();
+    const cityName = searchInput.value.trim();
+    if (cityName) {
+        displayCurrent(cityName);
+        displayForecast(cityName);
+        storedSearches.unshift(cityName);
+        localStorage.setItem("searches", JSON.stringify(storedSearches));
+        renderLocalStorage();
     }
 }
 
-function searchCityFromHistory(name) {
-    displayCurrent(name);
-    displayForecast(name);
-}
-
-searchHistory.addEventListener("click", function (e) {
-    console.log(e.target.textContent);
-    searchCityFromHistory(e.target.textContent);
+searchHistory.addEventListener("click", e => {
+    const cityName = e.target.textContent;
+    displayCurrent(cityName);
+    displayForecast(cityName);
 });
 
 searchForm.addEventListener("submit", searchCity);
+
+renderLocalStorage();
